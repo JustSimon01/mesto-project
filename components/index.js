01bd18ba42fbde3641439d58ecf5277914463654
JustSimon01 <simon.ch@yandex.ru>
2022-08-28 18:21:15 +0300

    модульная структура js файлов

---
+++ components/index.js
@@ -0,0 +1,42 @@
+import {initialCards} from './cards-massive.js';
+import {openPopup, closePopup, closeHotkey, closeOverlay, profileFirstUpload, submitProfileForm, profileIconEdit, openProfileEdit} from './modal.js';
+import {createCard, renderCard, addNewPlace} from './card.js';
+import {profileAddButton, cardPopupCloseButton, profileEditButton, profilePopupCloseButton, profileForm, nameInput, fullImagePopupClose, cardUploadForm, cardUploadPopup, cardTemplate, fullImagePopup, avatarEditCloseButton, profileAvatar, avatarEditPopup} from './utils.js';
+import {showInputError, hideInputError, isValid, setEventListeners, hasInvalidInput, toggleButtonState, enableValidation} from './validate.js';
+
+profileAddButton.addEventListener('click', function(){openPopup(cardUploadPopup)});
+cardPopupCloseButton.addEventListener('click', function(){closePopup(cardUploadPopup)});
+
+// Profile Edit Button
+profileEditButton.addEventListener('click', openProfileEdit);
+profilePopupCloseButton.addEventListener('click', function(){closePopup(profileEditPopup)});
+
+
+//Валидация и сохранение профиля
+
+//первичная подгрузка форм
+profileFirstUpload();
+
+profileForm.addEventListener('submit', submitProfileForm);
+nameInput.addEventListener('input', function (evt) {
+console.log(evt.target.validity.valid);
+}); 
+
+//валидация
+enableValidation();
+
+fullImagePopupClose.addEventListener('click', function(){closePopup(fullImagePopup)});
+
+//Загружаем массив карточек
+initialCards.forEach((element) =>{
+  renderCard(createCard(element.name, element.link), cardTemplate, false);
+});
+
+//Подгрузка карточек пользователем
+cardUploadForm.addEventListener('submit', addNewPlace);
+
+//редактирование профиля
+avatarEditCloseButton.addEventListener('click', function(){closePopup(avatarEditPopup)});
+profileAvatar.addEventListener('mouseover', profileIconEdit);
+profileAvatar.addEventListener('mouseout', profileIconEdit);
+profileAvatar.addEventListener('click', function(){openPopup(avatarEditPopup)});
---
+++ components/validate.js
@@ -0,0 +1,72 @@
+//Валидация и сохранение профиля
+
+//функции показа ошибки
+export const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  console.log (errorElement);
+  inputElement.classList.add('popup__input_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+export const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_error');
+  errorElement.textContent = '';
+  errorElement.classList.remove('popup__input-error_active');
+};
+//проверка валидности поля
+export const isValid = (formElement, inputElement) => {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity("");
+  }
+
+  if (!inputElement.validity.valid){
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  };
+};
+
+//слушатели событий для всех элементов формы
+export const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+  inputElement.addEventListener('input', () => {
+    isValid(formElement, inputElement);
+    toggleButtonState(inputList, buttonElement);
+  })
+})
+};
+
+//проверяем прошла ли валидация
+export const hasInvalidInput = (inputList) =>{
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+//отключение кнопки сохранения
+export const toggleButtonState = (inputList, buttonElement) =>{
+  if(hasInvalidInput(inputList)){
+    buttonElement.classList.add('popup__save-button_disabled');
+  } else {
+    buttonElement.classList.remove('popup__save-button_disabled');
+  }
+};
+
+//добавляем обработчики всем формам
+export const enableValidation = () =>{
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+  });
+  setEventListeners(formElement);
+});
+};
